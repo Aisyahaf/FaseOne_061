b795c890664cb85d7076ef0fe0c69a87fed373e1
Aisyah A Fitriani <aisyahafitriani05@gmail.com>
2024-03-15 20:06:33 +0700

    add layout form data makanan

--- lib/screen/datamakanan.dart
+++ lib/screen/datamakanan.dart
@@ -3,14 +3,14 @@ import 'package:ucp1_dataform/widget/footer_makanan.dart';
 import 'package:ucp1_dataform/widget/makanan_widget.dart';
 
 class MakananScreen extends StatelessWidget {
-  const MakananScreen({super.key, required this.nama, required this.no_telepon});
+  const MakananScreen(
+      {super.key, required this.nama, required this.no_telepon});
 
   final String nama;
   final String no_telepon;
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Data Makanan"),
@@ -27,9 +27,7 @@ class MakananScreen extends StatelessWidget {
             height: 15,
           ),
           MakananWidget(),
-          FooterMakanan(onPressedMakanan: (){
-            
-          })
+          FooterMakanan(onPressedMakanan: () {})
         ],
       ),
     );
