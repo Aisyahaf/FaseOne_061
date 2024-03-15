6a8fdf7730d531b88661baddb86be38ba35268c0
Aisyah A Fitriani <aisyahafitriani05@gmail.com>
2024-03-15 20:48:18 +0700

    Add Text Editing Controller

--- lib/widget/makanan_widget.dart
+++ lib/widget/makanan_widget.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
 class MakananWidget extends StatefulWidget {
-  const MakananWidget({super.key});
+  const MakananWidget(
+      {super.key,
+      required this.formKey,
+      required this.makanan,
+      required this.minuman,
+      required this.dessert});
+
+  final GlobalKey<FormState> formKey;
+  final TextEditingController makanan;
+  final TextEditingController minuman;
+  final TextEditingController dessert;
 
   @override
   State<MakananWidget> createState() => _MakananWidgetState();
@@ -11,41 +21,39 @@ class _MakananWidgetState extends State<MakananWidget> {
   @override
   Widget build(BuildContext context) {
     return Form(
-      child: Column(
-        children: [
-          TextFormField(
-            keyboardType: TextInputType.text,
-            decoration: const InputDecoration(
+        child: Column(
+      children: [
+        TextFormField(
+          controller: widget.makanan,
+          keyboardType: TextInputType.text,
+          decoration: const InputDecoration(
               labelText: "Makanan",
               hintText: "Masukkan Nama Makanan",
-              prefixIcon: Icon(Icons.fastfood_rounded)
-             ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          TextFormField(
-            keyboardType: TextInputType.text,
-            decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.fastfood_rounded)),
+        ),
+        SizedBox(
+          height: 25,
+        ),
+        TextFormField(
+          controller: widget.minuman,
+          keyboardType: TextInputType.text,
+          decoration: const InputDecoration(
               labelText: "Minuman",
               hintText: "Masukkan Nama Minuman",
-              prefixIcon: Icon(Icons.local_drink_rounded)
-             ),
-
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          TextFormField(
-            keyboardType: TextInputType.text,
-            decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.local_drink_rounded)),
+        ),
+        SizedBox(
+          height: 25,
+        ),
+        TextFormField(
+          controller: widget.dessert,
+          keyboardType: TextInputType.text,
+          decoration: const InputDecoration(
               labelText: "Dessert",
               hintText: "Masukkan Nama Dessert",
-              prefixIcon: Icon(Icons.cake_rounded)
-             ),
-
-          ),
-        ],
-      ));
+              prefixIcon: Icon(Icons.cake_rounded)),
+        ),
+      ],
+    ));
   }
 }
